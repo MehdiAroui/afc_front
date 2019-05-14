8b320bdb812245708040f2658548131a01a9084b
Mehdi Aroui <mehdipsy3@gmail.com>
2019-05-14 12:25:27 +0000

    new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FluxComponent } from './flux/flux.component';
-import { HomeComponent } from './home/home.component';
-
 const routes: Routes = [
-	{path: "", redirectTo: "/flux", pathMatch: "full"},
-	{path : "flux", component: FluxComponent},
-	{path : "home", component: HomeComponent},
-	{path : "dash", component: HomeComponent}
+	{path: "", redirectTo: "/flux", pathMatch: "full"}
 ];
 
 @NgModule({
