f247ae7b64f8dc301609880a1da88d7274f11231
Mehdi Aroui <mehdipsy3@gmail.com>
2019-05-14 13:52:18 +0000

    set auth UI

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+	
+	hide : boolean = true;
 
-  ngOnInit() {
-  }
+  	constructor(private authService : AuthService, public router: Router) { }
+
+  	ngOnInit() {
+  	}
+
+    getErrorMessage() {
+    	return this.email.hasError('required') ? 'You must enter a value' :
+        		this.email.hasError('email') ? 'Not a valid email' :
+            	'';
+  	}
+
+    login(){
+
+        let url = AuthService.redirectUrl;
+        let email = this.email.value,
+            password = this.password.value
+        let _that = this;
+
+        this.authService.makeQuery({ email,password}, true)
+          .subscribe(
+            data => _that.router.navigate(['/']))
+    }
+
+    signup(){
+      this.router.navigate(['/register'])
+    }
+
 
 }
